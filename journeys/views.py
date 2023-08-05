cd47debe528790166533f4e55fc729dc663960db
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2023-08-05 16:50:06 +0000

    refactored Profile and Journey views to generics

--- journeys/views.py
+++ journeys/views.py
@@ -1,73 +1,26 @@
-from django.http import Http404
-from rest_framework import status, permissions
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics, permissions
 from .models import Journey
 from .serializers import JourneySerializer
 from globetrotter_drf_api.permissions import IsOwnerOrReadOnly
 
 
-class JourneyList(APIView):
+class JourneyList(generics.ListCreateAPIView):
+    """
+    List journeys (posts) or create a post if logged in.
+    The perform_create method associates the post with the logged in user.
+    """
     serializer_class = JourneySerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
-    ]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Journey.objects.all()
 
-    def get(self, request):
-        journeys = Journey.objects.all()
-        serializer = JourneySerializer(
-            journeys, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = JourneySerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
+    def perform_create(self, serializer):
+        serializer.save(owner=.self.request.user)
 
 
 class JourneyDetail(APIView):
-    permission_classes = [IsOwnerOrReadOnly]
+    """
+    Retrieve a journey (post) and edit or delete it if you own it.
+    """
     serializer_class = JourneySerializer
-
-    def get_object(self, pk):
-        try:
-            journey = Journey.objects.get(pk=pk)
-            self.check_object_permissions(self.request, journey)
-            return journey
-        except Journey.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk):
-        journey = self.get_object(pk)
-        serializer = JourneySerializer(
-            journey, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        journey = self.get_object(pk)
-        serializer = JourneySerializer(
-            journey, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
-    
-    def delete(self, request, pk):
-        journey = self.get_object(pk)
-        journey.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Journey.objects.all()
