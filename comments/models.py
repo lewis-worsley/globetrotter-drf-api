cca2ddf4f186d347cf646f28053b29f6565ade2d
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2023-08-05 09:53:30 +0000

    created the likes model

--- comments/models.py
+++ comments/models.py
@@ -3,10 +3,9 @@ from django.contrib.auth.models import User
 from journeys.models import Journey
 
 
-
 class Comment(models.Model):
     """
-    Comment model, related to User and Post
+    Comment model, related to User and Journey.
     """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     journey = models.ForeignKey(Journey, on_delete=models.CASCADE)
--- globetrotter_drf_api/settings.py
+++ globetrotter_drf_api/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
     'comments',
     'journeys',
+    'likes',
     'profiles',
 ]
 
