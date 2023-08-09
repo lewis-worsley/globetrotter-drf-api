eb91a65a08a14726799b8b244424b4660c527305
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2023-08-09 21:15:20 +0000

    renamed url paths to reflect journeys activities

--- likes_stops/urls.py
+++ likes_stops/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from likes_stops import views
 
 urlpatterns = [
-    path('likesstops/', views.LikeStopList.as_view()),
-    path('likesstops/<int:pk>/', views.LikeStopDetail.as_view()),
+    path('stops/likes/', views.LikeStopList.as_view()),
+    path('stops/likes/<int:pk>/', views.LikeStopDetail.as_view()),
 ]
