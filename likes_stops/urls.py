6364975cbfab85d68b82f3b909223c55def57412
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2023-08-10 13:11:25 +0000

    url name changes for likes_stops

--- likes_stops/urls.py
+++ likes_stops/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from likes_stops import views
 
 urlpatterns = [
-    path('stops/likes/', views.LikeStopList.as_view()),
-    path('stops/likes/<int:pk>/', views.LikeStopDetail.as_view()),
+    path('journeys/stops/likes/', views.LikeStopList.as_view()),
+    path('journeys/stops/likes/<int:pk>/', views.LikeStopDetail.as_view()),
 ]
