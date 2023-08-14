931cf31123ffc17092ace70c4441947f3dd99b47
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2023-08-14 13:25:46 +0000

    added News app to the admin panel

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import News
+
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at', 'updated_at']
+    search_fields = ['title']
+    list_filter = ['created_at']
+
+    def __str__(self):
+        return f'{self.title}'
+
+admin.site.register(News, NewsAdmin)
