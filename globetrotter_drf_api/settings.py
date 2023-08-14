7d39fd21de4c931d1d46fb6e7de1fa1a1a41df96
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2023-08-14 18:21:48 +0000

    added root_route to API, pagination to all ListViews, a default JSON renderer for production, and the date time formatting for all the created_at and updated_at fields

--- globetrotter_drf_api/settings.py
+++ globetrotter_drf_api/settings.py
@@ -31,6 +31,10 @@ REST_FRAMEWORK = {
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     )],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DATETIME_FORMAT': '%d %b %Y',
 }
 if 'DEV' not in os.environ:
     REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
@@ -41,7 +45,6 @@ REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
-
 
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'globetrotter_drf_api.serializers.CurrentUserSerializer'
