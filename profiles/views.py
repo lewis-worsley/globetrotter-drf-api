616d2ea64337c260c6c838bd669a96cf7a2a54cf
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2023-08-22 12:10:42 +0000

    changed generics API view on ProfileDetail

--- profiles/views.py
+++ profiles/views.py
@@ -8,7 +8,7 @@ from .serializers import ProfileSerializer
 
 class ProfileList(generics.ListAPIView):
     """
-    List all profiles
+    List all profiles.
     No create view (post method), as profile creation handled by Django signals
     """
     serializer_class = ProfileSerializer
@@ -35,15 +35,15 @@ class ProfileList(generics.ListAPIView):
         'owner__followed__owner__profile', # 2
     ]
 
-class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
+class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
     Retrieve or update a profile if you're the owner
     """
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Profile.objects.annotate(
-        journeys_count = Count('owner__journey', distinct=True),
-        followers_count = Count('owner__followed', distinct=True),
-        following_count = Count('owner__following', distinct=True)
+        journeys_count=Count('owner__journey', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
     ).order_by('-created_at')
     
